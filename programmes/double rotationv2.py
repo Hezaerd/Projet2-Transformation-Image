be3f53ac315a6169a6c4c8d07f98d43ef264d4fa
Hezaerd <56612709+Hezaerd@users.noreply.github.com>
2021-04-16 21:26:49 -0400

    Modification commentaires première transfo

--- programmes/double rotationv2.py
+++ programmes/double rotationv2.py
@@ -1,45 +1,48 @@
-import os, sys # importer
+# -- Dylan Jean-Baptiste & Dylan Kesler & Swann Rouanet -- #
+
+# Les librairies qu'on utilise sont : os, system et PIL
+import os, sys
 from PIL import Image
+
+
+# On ouvre l'image.
 img = Image.open('47a.jpg')
 
-dup = Image.open('47a.jpg')
+# On crée une copie de l'image.
+dup = img
 
+# On récupère les informations de dimentions de l'image.
 ligne, colonne = img.size
 
-print(colonne)
-print(ligne)
-
-
-tableaupix=img.load()#charger l'image dans une matrice de pixels
-tableaupix2=dup.load()
+# On charge les deux images dans une matrice de pixels réspective.
+tableaupix=img.load() # Image originale
+tableaupix2=dup.load() # Copie de l'image
 
 
 
 
-def doublerotation(rotat): # le nombre de rotation correspond au nombre  de ligne ou de colonne  ppcm(largeur,hauteur)
-    for a in range(rotat):# permet de trouver la periode
+def doublerotation(rotat): # Le nombre de rotation correspond au nombre de ligne ou de colonne soit : ppcm(largeur,hauteur).
+    for a in range(rotat): # Permet de trouver la periode.
         for j in range(colonne):
             for i in range(ligne):
-                tableaupix2[i,j]=(255, 255, 255)# je met mon image a blanc
+                tableaupix2[i,j]=(255, 255, 255) # On transforme l'image en un rectangle blanc.
         for j in range(colonne):
             for i in range(ligne):
-                if i+1 == ligne and j+1 < colonne:# traitement de la derniere ligne de  l'image sauf la derniere case
+                if i+1 == ligne and j+1 < colonne: # On traite la derniere ligne de l'image sauf la derniere case.
                     tableaupix2[0,j+1]=tableaupix[i,j]
-                if j+1 == colonne and i+1 < ligne: # traitement de la derniere colonne sof la derniere case
+                if j+1 == colonne and i+1 < ligne: # On traite la derniere colonne sauf la derniere case.
                     tableaupix2[i+1,0]=tableaupix[i,j]
-                if i+1 == ligne and j+1 == colonne:# Traitement de la derniere case
+                if i+1 == ligne and j+1 == colonne: # On traite la derniere case.
                     tableaupix2[0,0]=tableaupix[i,j]
-                if i+1 < ligne and j+1 < colonne:# traitement de toute les autres case
+                if i+1 < ligne and j+1 < colonne: # On traite toutes les autres case.
                     tableaupix2[i+1,j+1]=tableaupix[i,j]
         for j in range(colonne):
             for i in range(ligne):
-                tableaupix[i,j]=tableaupix2[i,j]# copie le tableau de pixels dans celui de depart
-        if a%20==0:# affiche une image toute les 20 rotation
-            dup.show()
-    dup.show()# affiche l'image obtenue
+                tableaupix[i,j]=tableaupix2[i,j] # On copie le tableau de pixels dans celui de depart.
+    dup.show() # affiche le résultat finale 
 
-#Pour chaque pixel, si son numéro de ligne est pair, on l'augmente de 2, s'il est impair on le diminue de 2.
-#Même chose pour la colonne. Contrainte de taille d'image : hauteur et largeur paires. Période : ppcm(larg/2, haut/2)
+# Pour chaque pixel, si son numéro de ligne est pair, on l'augmente de 2, s'il est impair on le diminue de 2.
+# Même chose pour la colonne. Contrainte de taille d'image : hauteur et largeur paires. Période : ppcm(larg/2, haut/2)
 def rotX(rotat):# ppcm(ligne//2,colonne//2), l'image doit avoir un nombre de ligne et de colonne pair
     for a in range(rotat):
         for j in range(colonne):
